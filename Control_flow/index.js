7215fe8cfaa71207bc14ced90bb35424fdb47265
shshrukh <shshrukh.arbaz@gmail.com>
2024-06-29 23:27:28 +0500

    making if statement for cheaking the student is pass or fail

---
+++ Control_flow/index.js
@@ -0,0 +1,8 @@
+let passingMarkes= 50;
+let obtaningMarkes = prompt("mention your obtaning marks!");
+
+if (obtaningMarkes >= passingMarkes) {
+    console.log("Conguratulation you are pass")
+} else{
+    console.log("try your best next time")
+}
