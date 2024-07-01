9843d31abf15e5a70f8bdfe14e7c632406433f50
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-02 02:47:12 +0500

    adding return statement in wheather comment

--- Functions_and_scope/index.js
+++ Functions_and_scope/index.js
@@ -17,3 +17,26 @@ function aboutTemperatureComment(x , y) {
     }
 }
 aboutTemperatureComment(currentWeather , currentTemperature); 
+
+// using return statement in same above function
+
+let currentWeatherr = prompt ("What is your current weather status? sunny, rainy?");
+let currentTemperaturee = Number(prompt ("what is your currnet temperature status?"));
+
+function aboutTemperatureComment(x , y) {
+    
+    let avegTemp =25;
+    if (x =="sunny" && avegTemp <= y && y <= 50 ) {
+        return "drink more water to maintain your temperature! ";
+    } else if  ( x =="rainy" && y <= avegTemp) {
+       return "use warm food";
+    } else if ( x == "sunny" || y < avegTemp) {
+        return "enjoy the sunny day";
+    }else if ( x == "rainy" || y > avegTemp){
+        return "enjoy the rainy day";
+    }else {
+        "sorry wrong input";
+    }
+    
+}
+ console.log (aboutTemperatureComment(currentWeatherr , currentTemperaturee));
