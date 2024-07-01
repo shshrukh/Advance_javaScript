af03cfb8804b7f25825fdd092caf77ec2da957d0
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-02 00:24:12 +0500

    comment accounding to current temperature

--- Control_flow/index.js
+++ Control_flow/index.js
@@ -6,3 +6,20 @@ if (obtaningMarkes >= passingMarkes) {
 } else{
     console.log("try your best next time")
 }
+
+// making temperature and comment how to responce
+
+let avegTemp =25;
+let currentWeather = prompt ("What is your current weather status? sunny, rainy?");
+let currentTemperature = Number(prompt ("what is your currnet temperature status?"));
+if (currentWeather==="sunny" && avegTemp <= currentTemperature && currentTemperature <= 50 ) {
+    console.log ("drink more water to maintain your temperature! ");
+} else if  ( currentWeather === "rainy" && currentTemperature <= avegTemp) {
+    console.log("use warm food");
+} else if ( currentWeather === "sunny" || currentTemperature < avegTemp) {
+    console.log ("enjoy the sunny day");
+}else if ( currentWeather === "rainy" || currentTemperature > avegTemp){
+    console.log ("enjoy the rainy day");
+}else {
+    console.log("sorry not required input");
+}
