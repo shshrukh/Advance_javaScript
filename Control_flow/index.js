ed11a126a67d2445925dc4fb6dc0d6dde6de0cf7
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-02 00:33:52 +0500

    removing === comparision to == comparision operation

--- Control_flow/index.js
+++ Control_flow/index.js
@@ -12,13 +12,13 @@ if (obtaningMarkes >= passingMarkes) {
 let avegTemp =25;
 let currentWeather = prompt ("What is your current weather status? sunny, rainy?");
 let currentTemperature = Number(prompt ("what is your currnet temperature status?"));
-if (currentWeather==="sunny" && avegTemp <= currentTemperature && currentTemperature <= 50 ) {
+if (currentWeather =="sunny" && avegTemp <= currentTemperature && currentTemperature <= 50 ) {
     console.log ("drink more water to maintain your temperature! ");
-} else if  ( currentWeather === "rainy" && currentTemperature <= avegTemp) {
+} else if  ( currentWeather == "rainy" && currentTemperature <= avegTemp) {
     console.log("use warm food");
-} else if ( currentWeather === "sunny" || currentTemperature < avegTemp) {
+} else if ( currentWeather == "sunny" || currentTemperature < avegTemp) {
     console.log ("enjoy the sunny day");
-}else if ( currentWeather === "rainy" || currentTemperature > avegTemp){
+}else if ( currentWeather == "rainy" || currentTemperature > avegTemp){
     console.log ("enjoy the rainy day");
 }else {
     console.log("sorry not required input");
