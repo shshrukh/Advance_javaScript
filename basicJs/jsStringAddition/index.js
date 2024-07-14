1b47f1fc7d4506e689e30a95fb3f3e3e2fcf4059
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-14 23:13:27 +0500

    adding or concatenate string by += operater

--- basicJs/jsStringAddition/index.js
+++ basicJs/jsStringAddition/index.js
@@ -5,3 +5,12 @@ let result = "Hello"+" "+fNmae+" "+lNmae;
 let result2 =document.getElementById("demo");
 
 result2.innerText=result
+
+// The += assignment operator also be use to add (concatenate) strings:
+
+let textOne = "hell to ";
+textOne += "Java Script"
+let result3 = document.querySelector("#demoTwo");
+
+result3.innerHTML=textOne;
+console.log(result3.innerText=textOne)
