c27a5a47a9fc2e3f9fd8ab8884e6cc535b4e1621
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-14 23:32:58 +0500

    adding number with numbe and  number with string

--- basicJs/jsStringAddition/index.js
+++ basicJs/jsStringAddition/index.js
@@ -9,8 +9,22 @@ result2.innerText=result
 // The += assignment operator also be use to add (concatenate) strings:
 
 let textOne = "hell to ";
-textOne += "Java Script"
+textOne += "Java Script";
 let result3 = document.querySelector("#demoTwo");
 
 result3.innerHTML=textOne;
-console.log(result3.innerText=textOne)
+
+const textTwo = 5;
+const textThree = '5';
+const textFour= 'hello';
+let resultOne = textTwo + textTwo;
+let resultTwo = textTwo + textThree;
+let resultThree = textTwo + textFour;
+
+let result4 =document.getElementById("demoThree");
+let result5 =document.getElementById("demoFour");
+let result6 =document.getElementById("demoFive");
+result4.innerText=resultOne;
+result5.innerText=resultTwo;
+result6.innerText=resultThree;
+
