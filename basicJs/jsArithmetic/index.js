d276bdf35aaa9d5edb1bc6c5914d6eff74fc379e
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-15 20:46:17 +0500

    changes in variables

--- basicJs/jsArithmetic/index.js
+++ basicJs/jsArithmetic/index.js
@@ -1,23 +1,25 @@
-const textOne = 2;
-const textTwo = 3;
-const textThree = 50;
-const textFour = 7;
-let textFive = 9;
+const numberOne = 2;
+const numberTwo = 3;
+const numberThree = 50;
+const numberFour = 7;
+let numberFive = 9;
+numberFive++;
+let numberSix = 10;
+numberSix--;
 
-let additionResult = textOne + textTwo;
-let subtractionResult = textThree - textFour; 
-let multplicationResult = textFour * textOne;
-let divisionResult = textThree / textOne;
-let exponentiationResult = textOne ** textTwo
-let modulusResult = textFour % textTwo;
-textFive++;
-textFive--;
+let additionResult = numberOne + numberTwo;
+let subtractionResult = numberThree - numberFour; 
+let multiplicationResult = numberFour * numberOne;
+let divisionResult = numberThree / numberOne;
+let exponentiationResult = numberOne ** numberTwo
+let modulusResult = numberFour % numberTwo;
 
-document.getElementById("demoOne").innerText = textOne+" + "+textTwo+" = "+ additionResult;
-document.getElementById("demoTwo").innerText = textThree+" - "+textFour+" = "+ subtractionResult;
-document.getElementById("demoThree").innerText = textFour+" * "+textOne+" = "+ multplicationResult;
-document.getElementById("demoFour").innerText = textThree+" / "+textOne+" = "+ divisionResult;
-document.getElementById("demoFive").innerText = textOne+" ** "+textTwo+" = "+ exponentiationResult;
-document.getElementById("demoSix").innerText = textFour+" % "+textTwo+" = "+ modulusResult;
-document.getElementById("demoSeven").innerText = textFive+" + 1 = "+ textFive;
-document.getElementById("demoEight").innerText = textFive+" - 1 = "+ textFive;
+
+document.getElementById("demoOne").innerText = numberOne+" + "+numberTwo+" = "+ additionResult;
+document.getElementById("demoTwo").innerText = numberThree+" - "+numberFour+" = "+ subtractionResult;
+document.getElementById("demoThree").innerText = numberFour+" * "+numberOne+" = "+ multiplicationResult;
+document.getElementById("demoFour").innerText = numberThree+" / "+numberOne+" = "+ divisionResult;
+document.getElementById("demoFive").innerText = numberOne+" ** "+numberTwo+" = "+ exponentiationResult;
+document.getElementById("demoSix").innerText = numberFour+" % "+numberTwo+" = "+ modulusResult;
+document.getElementById("demoSeven").innerText = "9  + 1 = "+numberFive;
+document.getElementById("demoEight").innerText = "10  - 1 = "+ numberSix;
