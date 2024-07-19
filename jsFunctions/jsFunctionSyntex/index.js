c2258f1722f57697eb3d4c795bd7bff47d24ddb5
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-19 23:37:47 +0500

    making function and console the values of function

---
+++ jsFunctions/jsFunctionSyntex/index.js
@@ -0,0 +1,12 @@
+function jsFunctionSyntax(){
+    console.log("Hello to JS");
+}
+// function ivocation
+ jsFunctionSyntax();
+//  the result will be in console.log is Hello to JS and data type is number.
+
+function addition(){
+    console.log(1 *2);
+}
+addition();
+// the result will be in console.log is 2 and data type is number.
