9cb8c19995d1a7cf4e2f8d83db90f2a8af4e877e
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-19 23:55:22 +0500

    using arguments and parameters in function

--- jsFunctions/jsFunctionSyntex/index.js
+++ jsFunctions/jsFunctionSyntex/index.js
@@ -19,3 +19,11 @@ let resultOne = multplication();
 console.log(resultOne);
  
 // when we call or invoked the function it will excicute and if we used return keyword then it will stope excution of rset of the code after return keyword and return the value
+
+// Parameters and arguments 
+ function subtraction(parameter1, parameter2){
+    return parameter1 - parameter2;
+ }
+ let resultTow = subtraction(5 - 3);
+ console.log(resultTow);
+//  here when we define the function and inside the round brackets we define the parameters and using that parameters in function or function scope and when we call or invoked the function we define arguments. Arguments can hold the calue of parameters
