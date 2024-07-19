eb18a051496182d1a4b52ec15580f2f5153e161f
shshrukh <shshrukh.arbaz@gmail.com>
2024-07-19 23:47:22 +0500

    using return keyword in function

--- jsFunctions/jsFunctionSyntex/index.js
+++ jsFunctions/jsFunctionSyntex/index.js
@@ -10,3 +10,12 @@ function addition(){
 }
 addition();
 // the result will be in console.log is 2 and data type is number.
+
+// return
+function multplication(){
+    return 4 * 5 ;
+}
+let resultOne = multplication();
+console.log(resultOne);
+ 
+// when we call or invoked the function it will excicute and if we used return keyword then it will stope excution of rset of the code after return keyword and return the value
