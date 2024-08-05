4ca37f69aa666d40fe10238da75604c431156d44
shshrukh <shshrukh.arbaz@gmail.com>
2024-08-05 13:04:40 +0500

    some more details about primitive data typw

--- callBackHells/logic.js
+++ callBackHells/logic.js
@@ -64,5 +64,94 @@ console.log(calculater(16,4,div));
 //------------------> callback Hell <----------------//
 
 
+// //let we have headings and we want change its innerContent text-color with respect to time after two seconds
+
+// ist we will triger the headings
+const heading_One = document.querySelector('.heading1');
+const heading_Two = document.querySelector('.heading2');
+const heading_Three = document.querySelector('.heading3');
+const heading_Four = document.querySelector('.heading4');
+const heading_Five = document.querySelector('.heading5');
+const heading_Six = document.querySelector('.heading6');
+const heading_Seven = document.querySelector('.heading7');
+const heading_Eight = document.querySelector('.heading8');
+
+function changing(a,b,c,d){
+    setInterval(() => {
+       a.textContent = b;
+       a.style.color = c; 
+    }, d);
+}
+changing(heading_One,"Arbaz","red",2000);
+changing(heading_Two,"Rashid","green",4000);
+changing(heading_Three,"Ata","yellow",6000);
+changing(heading_Four,"Hasnain","pink",8000);
+changing(heading_Five,"Khayam","gray",10000);
+changing(heading_Six,"Azlan","orange",12000);
+changing(heading_Seven,"Shayan","hotpink",14000);
+changing(heading_Eight,"Noor","brown",16000);
 
 
+
+const headingOne = document.querySelector(".heading-1");
+const headingTwo = document.querySelector(".heading-2");
+const headingThree = document.querySelector(".heading-3");
+const headingFour = document.querySelector(".heading-4");
+const headingFive = document.querySelector(".heading-5");
+const headingSix = document.querySelector(".heading-6");
+const headingSeven = document.querySelector(".heading-7");
+const headingEight = document.querySelector(".heading-8");
+
+//this below is called callback hells or paramid of Dome
+
+setTimeout(() => {
+    headingOne.textContent ="Saleem";
+    headingOne.style.color ="red";
+    setTimeout(() => {
+        headingTwo.textContent = "Naeem";
+        headingTwo.style.color = "blue";
+        setTimeout(() => {
+            headingThree.textContent = "Karim";
+            headingThree.style.color = "green";
+            setTimeout(() => {
+                headingFour.textContent = "Sultan";
+                headingFour.style.color = "orange";
+                setTimeout(() => {
+                    headingFive.textContent = "Sundar";
+                    headingFive.style.color = "gray";
+                    setInterval(() => {
+                        headingSix.textContent = "Mir";
+                        headingSix.style.color = "pink";
+                        setTimeout(() => {
+                            headingSeven.textContent = "Dr.Anjum";
+                            headingSeven.style.color = "hotpink";
+                            setTimeout(()=> { 
+                                headingEight.textContent = "Mubarak";
+                                headingEight.style.color = "yellow";
+                            }, 2000);    
+                        }, 2000);    
+                    }, 2000);    
+                }, 2000);    
+            }, 2000);    
+        }, 2000);    
+    }, 2000);    
+}, 2000);    
+
+
+function getData(id_one,id_two,time) {
+    setTimeout(()=>{
+        console.log('data',id_one);
+        setTimeout(()=>{
+            console.log('data',id_two);
+            setTimeout(() => {
+                for(x = 3; x <= 10; x++){
+                    setTimeout(()=>{
+                        console.log('data',x)
+                    }, time)
+                }
+            }, time);
+        }, time)
+    }, time)
+}
+
+getData(1,2,2000);
