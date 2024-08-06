d4bb78157961ab0910fa298dc35ffa4c0a208187
shshrukh <shshrukh.arbaz@gmail.com>
2024-08-06 11:38:56 +0500

    details about functions and there types

--- jsFunctions/jsFunctionSyntex/index.js
+++ jsFunctions/jsFunctionSyntex/index.js
@@ -8,7 +8,39 @@ function jsFunctionSyntax(){
 
 //types of function
 //---------->Function Declaration<-----------//
+// the function are acutally anonymous function (function with out name);
+
 function addition(numOne,numTow) {
     console.log(numOne + numTow);
 }
-addition();
+addition(1,3); 
+
+function multplication(numOne,numTow) {
+    return numOne *numTow
+}
+multplication(4,6);
+
+//-------->Function Expression<---------//
+const x = function(numOne,numTow){
+    return numOne - numTow
+}
+x(3,2);
+
+//--------->The Function() Constructor<--------//
+let myFynctionOne = new Function (a, b, 'return a * b');
+console.log(myFynctionOne(3,2)); //same as
+
+let myFunctionTwo =  function (a, b){
+    return a * b;
+}
+console.log(myFunctionTwo(3,2));
+
+//----------->Self-Invoking functions<-----------//
+// a self invoking expression started atomatically, without being called. 
+(function(){
+    console.log('Hello World')
+})();
+//The function above is actually an anonymous self-invoking function (function without name).
+
+//-------->Important Note<---------//
+
